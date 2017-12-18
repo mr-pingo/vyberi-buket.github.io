2fbfb5469bcb8dfbcee464938cc40858b8232bad
Иван <ivan10.01.96@gmail.com>
2017-12-18 17:27:08 +0300

    18 декабря 2017

---
+++ template/js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    //-------
+    // Slider
+    //-------
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+    //-------
+    // Select
+    //-------
+    $('select').ikSelect();
+});
